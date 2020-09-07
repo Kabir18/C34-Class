39283a14083045f13d7bab1a385af918659c7af3
Kabir18 <64348385+Kabir18@users.noreply.github.com>
2020-09-07 19:43:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var movingball,database,position;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    movingball = createSprite(250,250,10,10);
+    movingball.shapeColor = "red";
+
+    var movingballposition = database.ref('ball/position')
+    movingballposition.on("value",readposition,showerror)
+    
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+    if(keyDown(LEFT_ARROW)){
+        writeposition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writeposition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writeposition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writeposition(0,+1);
+    }
+    drawSprites();
+}
+}
+
+
+
+function readposition(data){
+ position = data.val()
+ movingball.x = position.x
+ movingball.y = position.y 
+}
+function writeposition(x,y){
+database.ref('ball/position').set({
+'x':position.x+x,
+'y':position.y+y
+})
+}
+function showerror(){
+    console.log('I am back to coding')
+}
